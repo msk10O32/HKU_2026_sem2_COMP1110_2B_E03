d9bc760dd6fd837f6d440a8b7ebb8a2fed941823
wuxie2668 <ericlu2021@163.com>
2026-04-10 19:17:36 +0800

    some amendment in filter.py and generate.py

--- filter.py
+++ filter.py
@@ -3,7 +3,7 @@ import datetime as dt
 '''this file aims to read the input values from the input.csv file,
 filter the input values based on the defined rules, 
 determine the table type based on the granularity levels defined in the granularity.csv file, 
-and convert the created_at time to a dict with hour and minute. 
+and convert the arrival_time time to a dict with hour and minute. 
 The output is printed as a list of dicts.
 '''
 #functionized error checking for the input values
@@ -21,22 +21,22 @@ def is_valid_granularity(row):
 #check input values and assign the table type based on the granularity levels
 def is_valid_input(row):
     try:
-        if int(row['custom_count']) < 0:
-            raise ValueError(f"Custom count must be a non-negative integer: row: {row}")
+        if int(row['group_size']) < 0:
+            raise ValueError(f"Group size must be a non-negative integer: row: {row}")
     except:
-            raise TypeError(f"Invalid value for custom count: row: {row}")
+            raise TypeError(f"Invalid value for group size: row: {row}")
     try:
-        if int(row['time_cost']) < 0:
-            raise ValueError(f"Time cost must be a non-negative integer: row: {row}")
+        if int(row['dining_duration']) < 0:
+            raise ValueError(f"Dining duration must be a non-negative integer: row: {row}")
     except:
-            raise TypeError(f"Invalid value for time cost: row: {row}")
+            raise TypeError(f"Invalid value for dining duration: row: {row}")
     try:
-        dt.datetime.strptime(row['created_at'], '%Y-%m-%d %H:%M:%S')
+        dt.datetime.strptime(row['arrival_time'], '%Y-%m-%d %H:%M:%S')
     except:
-        raise ValueError(f"Invalid date format for created_at: row: {row['created_at']}")
+        raise ValueError(f"Invalid date format for arrival_time: row: {row['arrival_time']}")
     VIP = {0,1}
     try:
-        if int(row['vip']) not in VIP:
+        if int(row['is_vip']) not in VIP:
             raise ValueError(f"Invalid VIP value: row: {row}")
     except:
         raise TypeError(f"Invalid VIP value: row: {row}")
@@ -44,7 +44,7 @@ def is_valid_input(row):
 
 def main():
     #initialize the csv file with the granularity levels
-    with open('granularity.csv', 'r') as file:
+    with open('data\\granularity.csv', 'r') as file:
         reader = csv.DictReader(file)
         granularity_levels = []
         #remove duplicate split points and check for invalid values, then sort the granularity levels based on the split points
@@ -63,26 +63,26 @@ def main():
             buffer = dict()
             #check input values and assign the table type based on the granularity levels
             if is_valid_input(row):
-                buffer['custom_count'] = int(row['custom_count'])
-                buffer['time_cost'] = int(row['time_cost'])
-                buffer['created_at'] = row['created_at']
-                buffer['vip'] = int(row['vip'])
+                buffer['group_size'] = int(row['group_size'])
+                buffer['dining_duration'] = int(row['dining_duration'])
+                buffer['arrival_time'] = row['arrival_time']
+                buffer['is_vip'] = int(row['is_vip'])
             #if the custom count is greater than or equal to the largest split, assign the largest table type
-            if int(row['custom_count']) >= granularity_levels[-1][1]:
+            if int(row['group_size']) >= granularity_levels[-1][1]:
                 buffer['table_type'] = granularity_levels[-1][0]
             else:
                 for level in range(len(granularity_levels)-1):
-                    if int(row['custom_count']) in range(granularity_levels[level][1], granularity_levels[level+1][1]):
+                    if int(row['group_size']) in range(granularity_levels[level][1], granularity_levels[level+1][1]):
                         buffer['table_type'] = granularity_levels[level][0]
                         break
             output.append(buffer)
-        output.sort(key=lambda x: x['created_at'])
+        output.sort(key=lambda x: x['arrival_time'])
 
         #output the request dicts to a csv file with assigned uid, in case some or the requests are missing
-        with open('request.csv','w',newline='') as file:
+        with open('data\\request.csv','w',newline='') as file:
             i = 0
             size = len(str(len(output)))
-            fieldnames = ['uid','custom_count', 'created_at', 'time_cost', 'table_type', 'vip']
+            fieldnames = ['uid','group_size', 'arrival_time', 'dining_duration', 'table_type', 'is_vip']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
             for row in output:
--- generate.py
+++ generate.py
@@ -1,13 +1,12 @@
 # module1 : customer data generater (generate.py)
 #function: generate large amount of raw data for simulation and output as input.csv
-
 import random
 import csv
 from datetime import datetime,timedelta
 
 #The following are parameters that allow for customizable ratios.
 Total_groups = 100 #total amount of customer group
-Arrival_time_range = (0,100) #Customer arrival time is categorized into ranges (unit: minutes, 0 = simulated start time, 120 = 2-hour peak).
+created_at_range = (0,100) #Customer arrival time is categorized into ranges (unit: minutes, 0 = simulated start time, 120 = 2-hour peak).
 Group_size_range = (1,6) #Group size range (currently preset to 1-6 people)
 Dining_duration_range = (20,60) #Meal time range (currently preset to 20-60 minutes)
 Vip_ratio = 0.1 #VIP customer ratio (currently set at 10%)
@@ -17,31 +16,31 @@ BASE_TIME = datetime(2024,6,1,0,0,0) #Base time: 2024-06-01 00:00:00
 def main():
     random.seed(Random_seed)
 
-    #generate customer raw data
+    # generate customer raw data
     customer_data = []
     for i in range(Total_groups):
         group_size = random.randint(*Group_size_range)
-        arrival_min = random.randint(*Arrival_time_range)
+        arrival_min = random.randint(*created_at_range)
         dining_duration = random.randint(*Dining_duration_range)
         is_vip = random.random() < Vip_ratio #Generate VIP according to the preset ratio
 
-        arrival_time = (BASE_TIME + timedelta(minutes=arrival_min)).strftime("%Y-%m-%d %H:%M:%S")
+        created_at = (BASE_TIME + timedelta(minutes=arrival_min)).strftime("%Y-%m-%d %H:%M:%S")
 
         customer_data.append({
             "group_size": group_size,
-            "arrival_time": arrival_time,
+            "created_at": created_at,
             "dining_duration": dining_duration,
             "is_vip": 1 if is_vip else 0
         })
 
-        #Sort by time to ensure customers are seated in order from bottom to top.
-    customer_data.sort(key=lambda x: x["arrival_time"])
+        # Sort by time to ensure customers are seated in order from bottom to top.
+    customer_data.sort(key=lambda x: x["created_at"])
 
-        #input input.csv file
-    output_file = "input.csv"
+    #input input.csv file
+    output_file = "data\\input.csv"
     with open(output_file,"w",newline="",encoding="utf-8") as f:
-        #Set table column names
-        writer = csv.DictWriter(f, fieldnames=["group_size", "arrival_time", "dining_duration", "is_vip"])
+        # Set table column names
+        writer = csv.DictWriter(f, fieldnames=["group_size", "created_at", "dining_duration", "is_vip"])
         writer.writeheader()
         writer.writerows(customer_data)
 
