d99ce93ab6a6631a6e42a034fba8165fe6260dcf
msk10O32 <u3659030@connect.hku.hk>
2026-04-15 03:37:12 +0800

    fixed queuing issue

--- simulation.py
+++ simulation.py
@@ -17,27 +17,30 @@ def load_group():
     return group
 class Restaurant:
     def __init__(self,env,requests,groups):
-        self.queue_size = dict()
+        self.queue_sizes = dict()
         self.table_groups = dict()
         for group in groups:
-            self.table_groups[group["type"]] = simpy.PriorityResource(env, capacity=int(group["group_capacity"]))
-            self.queue_size[group["type"]] = float(group["queue_capacity"])
+            self.table_groups[group["type"]] = simpy.PriorityResource(env, capacity=int(group["table_capacity"]))
+            self.queue_sizes[group["type"]] = float(group["queue_capacity"])
         self.env = env
         self.requests = requests
         self.records = []
         self.standard_time = dt.datetime.strptime(self.requests[0]['arrival_time'], '%Y-%m-%d %H:%M:%S')
         self.end_time = 14400
     def run(self):
-        for request in self.requests:
-            self.env.process(self.handle_request(request))
+        self.env.process(self.process())
         self.env.run(until=self.end_time)
+    def process(self):
+        last = 0
+        for request in self.requests:
+            arrival_time = dt.datetime.strptime(request['arrival_time'], '%Y-%m-%d %H:%M:%S')
+            start = int((arrival_time-self.standard_time).total_seconds()/60)
+            print(f"Processing request {request['uid']}, arrives at {start} minutes")
+            yield self.env.timeout(start-last)
+            self.env.process(self.handle_request(request))
+            last = start
     def handle_request(self,request):
-        arrival_time = dt.datetime.strptime(request['arrival_time'], '%Y-%m-%d %H:%M:%S')
-        start = int((arrival_time-self.standard_time).total_seconds()/60)
-        print(f"Processing request {request['uid']}, arrives at {start} minutes")
-        if len(self.table_groups[request['table_type']].queue) <= self.queue_size[request['table_type']]:
-            yield self.env.timeout(start)
-        else:
+        if len(self.table_groups[request['table_type']].queue) >= self.queue_sizes[request['table_type']]:
             self.records.append({
                 "uid": request['uid'],
                 "group_size": request['group_size'],
@@ -53,7 +56,7 @@ class Restaurant:
         with self.table_groups[request['table_type']].request(priority=-int(request['is_vip'])) as req:
             yield req
             seating_time = self.env.now
-            print(f"Request {request['uid']} type: {request['table_type']} is served at time {seating_time}")
+            print(f"Request {request['uid']} type: {request['table_type']} is served at time {seating_time},waiting customers in queue: {len(self.table_groups[request['table_type']].queue)}")
             yield self.env.timeout(int(request['dining_duration']))
             leaving_time = self.env.now
             print(f"Request {request['uid']} type: {request['table_type']} is completed at time {leaving_time}")
